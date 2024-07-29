30e2814436cba7a13c7028a19c35f1b0b8de588c
unknown-29 <devgmehta4@gmail.com>
2024-07-30 01:56:01 +0530

    supporting valid strings of length > 1 separated by comma and also refactored the testcases

--- src/main/java/Calculator/Add.java
+++ src/main/java/Calculator/Add.java
@@ -8,6 +8,17 @@ public class Add {
         else if(numbers.length()==1 && numbers.charAt(0)>='0' && numbers.charAt(0)<='9'){
             return numbers.charAt(0)-'0';
         }
-        else return -1;
+        /* if string size > 1
+         * first split by delimiter ","
+         * second convert each char to int
+         * add
+         * return the result
+         */
+        String[] tokens=numbers.split(",");
+        int sum=0;
+        for (String token : tokens) {
+            sum+=Integer.valueOf(token);
+        }
+        return sum;
     }
 }
--- src/test/java/CalculatorTest/AddTest.java
+++ src/test/java/CalculatorTest/AddTest.java
@@ -6,9 +6,23 @@ import static Calculator.Add.add;
 
 public class AddTest {
     @Test
-    public void TestAddMethod(){
-        Assert.assertEquals(add(""),0);
-        Assert.assertEquals(add("1"),1);
-        Assert.assertEquals(add("2"),2);
+    public void testAddEmptyString() {
+        Assert.assertEquals(0, add(""));
+    }
+
+    @Test
+    public void testAddSingleNumber() {
+        Assert.assertEquals(1, add("1"));
+        Assert.assertEquals(2, add("2"));
+    }
+
+    @Test
+    public void testAddTwoNumbersSeparatedByComma() {
+        Assert.assertEquals(6, add("1,5"));
+    }
+
+    @Test
+    public void testAddMultipleNumbersSeparatedByComma() {
+        Assert.assertEquals(34, add("5,7,9,10,1,2"));
     }
 }
